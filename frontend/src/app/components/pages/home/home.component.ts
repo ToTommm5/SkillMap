4ba950bef0fb14fd43851d919b57e6540899d310
Tom <tom@moneret.fr>
2025-01-16 00:40:56 +0100

    telecharger cv + modif

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  ngOnInit(): void {}
+  constructor() {}
+  cvUrl = 'CV Moneret Tom 2025 .pdf';
 
+  DownloadCV() {
+    // Remplacez par l'URL de votre CV
+
+    const link = document.createElement('a');
+    link.href = this.cvUrl;
+    link.download = 'mon_cv.pdf'; // Nom du fichier lors du téléchargement
+    link.click();
+  }
 }
